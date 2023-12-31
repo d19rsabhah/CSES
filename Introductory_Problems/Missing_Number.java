e437c56cc4c82e6e5069a80fdd2fe70fcd501fe4
Rishav Das <rishavdas27602@gmail.com>
2023-12-31 23:52:39 +0530

    missing number

--- Introductory_Problems/Missing_Number.java
+++ Introductory_Problems/Missing_Number.java
@@ -2,20 +2,21 @@ import java.io.*;
 import java.util.*;
 import java.lang.*;
 
-class Missing_Number{
-	public static void main(String []args){
+public class Missing_Number {
+	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        ArrayList<Integer>list = new ArrayList<>();
-        for(int i = 0; i < n - 1; i++){
+		int n = sc.nextInt();
+		ArrayList<Integer> list = new ArrayList<>();
+		for (int i = 0; i < n - 1; i++) {
 			int x = sc.nextInt();
 			list.add(x);
-		}	
-      System.out.println(Solution(list, n));		
+		}
+		System.out.println(Solution(list, n));
 	}
-	private static int Solution(ArrayList<Integer>arr, int n){
+
+	private static int Solution(ArrayList<Integer> arr, int n) {
 		int N = n * (n + 1) / 2;
-		for(int i = 0; i < n - 1; i++){
+		for (int i = 0; i < n - 1; i++) {
 			N -= arr.get(i);
 		}
 		return N;
