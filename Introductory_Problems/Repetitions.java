1fbf0cf256198523c69f0cbcfad18212f221fd23
Rishav Das <rishavdas27602@gmail.com>
2023-12-31 23:53:31 +0530

    Repetations

---
+++ Introductory_Problems/Repetitions.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+
+public class Repetitions {
+	public static void main(String []args){
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+		int max_repetition = ;
+		int count = 1;
+		for(int i = 0; i < str.length() - 1; i++){
+			
+			if(str.charAt(i) == str.charAt(i + 1)){
+				count++;
+				if(count > max_repetition){
+					max_repetition = count;
+				}
+			} else {
+				count = 1;
+			}
+		}
+		System.out.println(max_repetition);
+	}
+}
