0bc7e68b99f2e6101f2860deae12b21b4811205d
Rishav Das <rishavdas27602@gmail.com>
2023-12-31 23:53:54 +0530

    Weird Algorithm

--- Introductory_Problems/Weird_Algorithm.java
+++ Introductory_Problems/Weird_Algorithm.java
@@ -2,15 +2,15 @@ import java.io.*;
 import java.lang.*;
 import java.util.*;
 
-class Weird_Algorithm{
-	public static void main(String []args){
+public class Weird_Algorithm {
+	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter a number : ");
 		int n = sc.nextInt();
-		ArrayList<Integer>list = new ArrayList<>();
+		ArrayList<Integer> list = new ArrayList<>();
 		list.add(n);
-		while(n != 1){
-			if(n % 2 == 0){
+		while (n != 1) {
+			if (n % 2 == 0) {
 				n /= 2;
 			} else {
 				n *= 3;
